40ef0ff4e35190c9fad07fa0a51d9d6ab453a8d8
gtseligas <71587056+gtseligas@users.noreply.github.com>
2023-01-13 19:59:06 +0200

    Add files via upload

--- full_gamato_api/index.js
+++ full_gamato_api/index.js
@@ -24,6 +24,19 @@ app.post('/admin/resetall', async(req, res) => {
     }
 });
 
+app.post('/admin/resetq/:questionnaireID', async(req, res) => {
+  try {
+    const id = req.params.questionnaireID;
+    
+    await pool.query("delete from qqso where questionnaire_myid in (select questionnaire_myid from questionnaire where questionnaire_id=?)", [id]);
+
+    // return status:OK JSON
+    res.json({"status":"OK"});
+  } catch (error) {
+    res.json({"status":"failed", "reason":error.message});
+  }
+});
+
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
   });
